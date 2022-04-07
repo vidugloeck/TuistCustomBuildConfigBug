95886e704fb7f77f8067183a6518317169d91682
Vidu Glöck <git@vidugloeck.com>
2022-04-07 16:11:56 +0200

    Add configurations to Dependencies.swift

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -1,9 +1,14 @@
 import ProjectDescription
-
 let dependencies = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.0.0")),
-    ],
+    swiftPackageManager:
+        SwiftPackageManagerDependencies([
+            .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.0.0")),
+        ],
+                                        baseSettings: .settings(configurations: [
+                                            .debug(name: "Debug"),
+                                            .release(name: "Release"),
+                                            .release(name: "Beta"),
+                                        ])),
     platforms: [.iOS]
 )
 
